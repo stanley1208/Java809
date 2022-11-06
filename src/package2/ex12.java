3c55429f52944bd2ba59de51c839818a9a9cb54e
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-11-06 17:40:28 +0800

    update

---
+++ src/package2/ex12.java
@@ -0,0 +1,22 @@
+package package2;
+
+public class ex12 {
+
+	public static void main(String[] args) {
+		int[] array= {1,2,3};
+		for(int i:array) {
+			System.out.print(i);
+		}
+//		for(int i=0;i<1;) {
+//			System.out.print(i);
+//		}
+//		for(;;) {
+//			
+//		}
+	}
+
+}
+
+
+
+
