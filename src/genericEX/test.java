5556e6018f9031c3443e2dee6577d3da01b52789
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-11-23 18:31:41 +0800

    updatte

---
+++ src/genericEX/test.java
@@ -0,0 +1,16 @@
+package genericEX;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+	public static void main(String[] args) {
+		List<String> l=new ArrayList<>();
+		l.add("abc");
+		l.add("rrr");
+		
+		for (String O:l) {
+			System.out.println(O);
+		}
+	}
+}
